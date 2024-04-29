bc2cb0d6532236858a314936c7480786ecdc0635
André Maciel Azevedo <andre.m.azevedo@tecnico.ulisboa.pt>
2024-04-29 15:07:50 +0100

    fix: function_definition_node

--- ast/function_definition_node.h
+++ ast/function_definition_node.h
@@ -3,6 +3,7 @@
 
 #include <cdk/ast/expression_node.h>
 #include <cdk/ast/sequence_node.h>
+#include "block_node.h"
 
 namespace til {
 
@@ -11,20 +12,18 @@ namespace til {
    */
   class function_definition_node : public cdk::expression_node {
     cdk::sequence_node *_arguments;
-    cdk::sequence_node *_declarations, *_instructions;
+    til::block_node *_block;
 
   public:
     function_definition_node(int lineno, std::shared_ptr<cdk::basic_type> funType, cdk::sequence_node *arguments, 
-                             cdk::sequence_node *declarations, cdk::sequence_node *instructions) :
-        cdk::expression_node(lineno), _arguments(arguments), _declarations(declarations), _instructions(instructions) {
+                             til::block_node *block) :
+        cdk::expression_node(lineno), _arguments(arguments), _block(block) {
       type(funType);
     }
 
     cdk::sequence_node *arguments() { return _arguments; }
 
-    cdk::sequence_node *declarations() { return _declarations; }
-
-    cdk::sequence_node *instructions() { return _instructions; }
+    til::block_node *block() { return _block; }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_function_definition_node(this, level); }
 
